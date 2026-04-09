6a159b5a808248d3e53085bd36b354aef6bab2b9
Alex <atravislewis@gmail.com>
2026-04-09 07:32:08 -0500

    Update CloudFront hostname after Amplify domain recreation
    Domain association was recreated (old was in FAILED state). New
    CloudFront distribution: d3oz9ge32cpzrg.cloudfront.net (was d398fplstgumie).

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -127,7 +127,7 @@ resource "aws_route53_record" "amplify_apex" {
   type    = "A"
 
   alias {
-    name                   = "d398fplstgumie.cloudfront.net"
+    name                   = "d3oz9ge32cpzrg.cloudfront.net"
     zone_id                = "Z2FDTNDATAQYW2"
     evaluate_target_health = false
   }
@@ -142,5 +142,5 @@ resource "aws_route53_record" "amplify_www" {
   name    = "www"
   type    = "CNAME"
   ttl     = 300
-  records = ["d398fplstgumie.cloudfront.net"]
+  records = ["d3oz9ge32cpzrg.cloudfront.net"]
 }
